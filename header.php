f561609e275b78d6ad1c752b353499553d298068
parksjg <joseph.parks@colorado.edu>
2018-05-03 11:14:55 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,22 @@
+<html>
+	<head>
+		<title> Secure Collective App </title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  		<meta name="description" content="">
+  		<meta name="author" content="">
+  		<link rel="stylesheet" href="bootstrap/css/bootstrap-theme.css">
+		<!-- Bootstrap core CSS -->
+    	<link href="bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    	<!-- Bootstrap theme -->
+    	<link href="bootstrap/dist/css/bootstrap-theme.min.css" rel="stylesheet">
+    	<!-- Custom styles for this template -->
+    	<link href="theme.css" rel="stylesheet">
+  		
+  		
+	</head>
+	<body>
+		<center>
+		
+		<b><h1> Collective </h1></b>
+		<h5> Secure Story Sharing </h5>
+		<hr>
