c67896c5db6117f6da982a3b02b2e69e57659c29
parksjg <joseph.parks@colorado.edu>
2018-05-03 11:14:25 -0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+#author: Joseph Parks joseph.parks@colorado.edu
+#name: index.php
+#purpose: Securing Story Sharing
+#date: 2018-5-1
+#version: 0.1
+
+include_once "/var/www/html/collective/header.php";
+
+echo "<a href=\"login.php\" class=\"btn btn-lg btn-primary\"> Go To Login </a>";
+
+include_once "/var/www/html/collective/footer.php";
+?>
